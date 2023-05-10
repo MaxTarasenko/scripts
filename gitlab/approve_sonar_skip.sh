bd818f8ecffc8e11003cd8018d2959eae5ceafcb
MaxTarasenko <50379518+MaxTarasenko@users.noreply.github.com>
2023-05-10 09:37:40 +0300

    Update approve_sonar_skip.sh

--- gitlab/approve_sonar_skip.sh
+++ gitlab/approve_sonar_skip.sh
@@ -5,14 +5,11 @@ approvals=$(curl -s --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" \
   "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}/approvals" | jq -r '.approved_by[].user.username')
 
 # Проверяем, есть ли пользователи из массива USERS_TO_CHECK среди проголосовавших пользователей
-SONAR_ABORT_PIPE="true"
 for user in "${USERS_TO_CHECK[@]}"; do
   for approved_user in ${approvals}; do
     if [[ "${approved_user}" == "${user}" ]]; then
-      SONAR_ABORT_PIPE="false"
+      export SONAR_ABORT_PIPE="false"
       break
     fi
   done
 done
-
-echo $SONAR_ABORT_PIPE
