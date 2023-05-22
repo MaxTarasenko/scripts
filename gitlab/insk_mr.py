04fad2ba0640964ce92b41c70ddb351fca34ff21
MaxTarasenko <50379518+MaxTarasenko@users.noreply.github.com>
2023-05-22 09:39:58 +0300

    Update insk_mr.py

--- gitlab/insk_mr.py
+++ gitlab/insk_mr.py
@@ -5,7 +5,7 @@ import os
 import gitlab
 
 # Gitlab Access
-url = os.environ.get('CI_API_V4_URL')
+url = os.environ.get('CI_SERVER_URL')
 token = os.environ.get('GITLAB_TOKEN')
 print(url, token)
 gl = gitlab.Gitlab(url, private_token=token)
