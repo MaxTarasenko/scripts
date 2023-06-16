ff7c825979e4e8474552c08771f714856d08db11
Maksym Tarasenko <maksym.tarasenko@chisw.com>
2023-06-16 20:50:50 +0300

    fix

--- gitlab/insk_mr.py
+++ gitlab/insk_mr.py
@@ -75,6 +75,9 @@ def sonar_skip():
             else:
                 print("export SONAR_ABORT_PIPE=true")
                 return
+        else:
+            print("export SONAR_ABORT_PIPE=true")
+            return
 
 
 # Get the passed command line arguments
